c30f09fa01cdceedc2a0915a4e623db4ae8fcb5b
Jahangir <jahangirhossainm69@gmail.com>
2021-10-24 02:42:58 +0600

    Initial commit

---
+++ SnippingMenu.py
@@ -0,0 +1,104 @@
+import sys
+from os.path import basename
+from PyQt5.QtCore import QPoint, Qt, QRect
+import webbrowser
+from PyQt5.QtWidgets import QAction, QMainWindow, QApplication, QPushButton, QMenu, QFileDialog
+from PyQt5.QtGui import QPixmap, QImage, QPainter, QPen
+
+import SnippingTool
+
+
+class Menu(QMainWindow):
+    default_title = "Bangla OCR Snipping Tool"
+
+    # numpy_image is the desired image we want to display given as a numpy array.
+    def __init__(self, numpy_image=None, snip_number=None, start_position=(1200, 600, 150, 50)):
+        super().__init__()
+
+        self.drawing = False
+        self.total_snips = 0
+        self.title = Menu.default_title
+
+        # New snip
+        new_snip_action = QAction('New', self)
+        new_snip_action.setShortcut('Ctrl+N')
+        new_snip_action.setStatusTip('Snip!')
+        new_snip_action.triggered.connect(self.new_snip_window)
+
+        # Exit
+        exit_window = QAction('Exit', self)
+        exit_window.setShortcut('Ctrl+Q')
+        exit_window.setStatusTip('Exit application')
+        exit_window.triggered.connect(self.close)
+
+        # About
+        about_action = QAction('About', self)
+        new_snip_action.setShortcut('Ctrl+N')
+        new_snip_action.setStatusTip('About')
+        about_action.triggered.connect(lambda: self.about())
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(new_snip_action)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(about_action)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(exit_window)
+
+        self.snippingTool = SnippingTool.SnippingWidget()
+        self.setGeometry(*start_position)
+
+        # From the second initialization, both arguments will be valid
+        if numpy_image is not None and snip_number is not None:
+            self.image = self.convert_numpy_img_to_qpixmap(numpy_image)
+            self.change_and_set_title("Bangla OCR Snipping Tool")
+        else:
+            self.image = QPixmap("background.PNG")
+            self.change_and_set_title(Menu.default_title)
+
+        self.resize(self.image.width(), self.image.height() + self.toolbar.height())
+        self.show()
+
+    # snippingTool.start() will open a new window, so if this is the first snip, close the first window.
+    def new_snip_window(self):
+        self.total_snips += 1
+        self.snippingTool.start()
+
+    def about(self):
+        self.setWindowTitle("Bangla OCR Snipping Tool")
+        print("------------------------")
+        print("Bangla OCR Snipping Tool")
+        print("------------------------")
+        print("------Developed By------")
+        print("----Jahangir Hossain----")
+        print("--------CSE,JnU---------")
+        print("---facebook.com/jhm69---")
+        webbrowser.open_new("https://facebook.com/jhm69")
+
+    def change_and_set_title(self, new_title):
+        self.title = new_title
+        self.setWindowTitle(self.title)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        rect = QRect(0, self.toolbar.height(), self.image.width(), self.image.height())
+        painter.drawPixmap(rect, self.image)
+
+    def mouseReleaseEvent(self, event):
+        if event.button == Qt.LeftButton:
+            self.drawing = False
+
+    # TODO exit application when we exit all windows
+    def closeEvent(self, event):
+        event.accept()
+
+    @staticmethod
+    def convert_numpy_img_to_qpixmap(np_img):
+        height, width, channel = np_img.shape
+        bytesPerLine = 3 * width
+        return QPixmap(QImage(np_img.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainMenu = Menu()
+    sys.exit(app.exec_())
---
+++ SnippingTool.py
@@ -0,0 +1,96 @@
+from tkinter import *
+import numpy as np
+import cv2
+from PIL import ImageGrab
+from PyQt5 import QtWidgets, QtCore, QtGui
+import SnippingMenu
+from PyQt5.QtCore import Qt
+import pytesseract
+import pyperclip3 as pc
+
+
+class SnippingWidget(QtWidgets.QWidget):
+    num_snip = 0
+    is_snipping = False
+    background = True
+
+    def __init__(self, parent=None):
+        super(SnippingWidget, self).__init__()
+        self.parent = parent
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        root = Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        self.setGeometry(0, 0, screen_width, screen_height)
+        self.begin = QtCore.QPoint()
+        self.end = QtCore.QPoint()
+
+    def start(self):
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        SnippingWidget.background = False
+        SnippingWidget.is_snipping = True
+        self.setWindowOpacity(0.3)
+        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
+        print('Capture the screen...')
+        print('Press q if you want to quit...')
+        self.show()
+
+    def paintEvent(self, event):
+        if SnippingWidget.is_snipping:
+            brush_color = (128, 128, 255, 100)
+            lw = 3
+            opacity = 0.3
+        else:
+            # reset points, so the rectangle won't show up again.
+            self.begin = QtCore.QPoint()
+            self.end = QtCore.QPoint()
+            brush_color = (0, 0, 0, 0)
+            lw = 0
+            opacity = 0
+
+        self.setWindowOpacity(opacity)
+        qp = QtGui.QPainter(self)
+        qp.setPen(QtGui.QPen(QtGui.QColor('black'), lw))
+        qp.setBrush(QtGui.QColor(*brush_color))
+        rect = QtCore.QRectF(self.begin, self.end)
+        qp.drawRect(rect)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Q:
+            print('Quit')
+            self.close()
+        event.accept()
+
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = self.begin
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        # SnippingWidget.num_snip = 0
+        SnippingWidget.is_snipping = False
+
+        x1 = min(self.begin.x(), self.end.x())
+        y1 = min(self.begin.y(), self.end.y())
+        x2 = max(self.begin.x(), self.end.x())
+        y2 = max(self.begin.y(), self.end.y())
+
+        self.repaint()
+
+        img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+
+        try:
+            pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+            text = pytesseract.image_to_string(img, lang="ben")
+            print(text)
+            pc.copy(text)
+        except Exception as e:
+            print(e)
+
+        # add to the snips list the object that opens a window of the image
+        # SnippingMenu.Menu(img, SnippingWidget.num_snip, (x1, y1, x2, y2))
