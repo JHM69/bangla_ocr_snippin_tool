0dc6deabc7ef7634b4a7e0c59dafe459bedf7764
Jahangir Hossain <jahangirhmarup@gmail.com>
2021-10-25 03:41:21 +0600

    Update multi_snipping_tool.py

--- multi_snipping_tool.py
+++ multi_snipping_tool.py
@@ -78,10 +78,6 @@ class MyWidget(QtWidgets.QWidget):
         print(img_name, 'saved')
         img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
 
-        # cv2.imshow('Captured Image', img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
